316ea0bc007fe6f74ea7f4292915e5dcffd293af
evvvritt <evv.info@gmail.com>
2021-06-01 16:42:05 +0200

    draft metadata (0, 3)

---
+++ metadata/index.js
@@ -0,0 +1,68 @@
+export default {
+  // titlePattern: 'Songs for the Contract {{no}}',
+  // editions: 3,
+  // generative: true,
+  description: 'David Rudnick. A collaboration with Folia',
+  // release: '03-05-2021 09:00:00 pm +01:00',
+  // directory: 'https://cloudflare-ipfs.com/ipfs/QmXiad6QM7GSxtCqcQa7aYJKVkbb62Qm6CCTHVhPcEA2ZS/FLA11000000__Emily-Segal__Code-of-Holes__2021',
+  // assetPath: '',
+  tokens: {
+    0: {
+      title: 'I &ndash; Transfixed By Zion'
+    },
+    1: {
+      title: 'II &ndash; The Quickest Jury Now'
+    },
+    2: {
+      title: 'III &ndash; The Ark Groans, Zags Up'
+    },
+    3: {
+      title: 'IV &ndash; Hyperspectral Dawn'
+    },
+    4: {
+      title: 'V &ndash; Zones Flex, Make New Ruins'
+    },
+    5: {
+      title: 'VI &ndash; Void Arrows, Zip Like'
+    },
+    6: {
+      title: 'VII &ndash; Jagged Quartz Towers'
+    },
+    7: {
+      title: 'VIII &ndash; Verdant Perjury'
+    },
+    8: {
+      title: 'IX &ndash; Nonreal Packet Maze'
+    },
+    9: {
+      title: 'X &ndash; Hacked Amazon'
+    },
+    10: {
+      title: 'XI &ndash; Quadratic Empire'
+    },
+    11: {
+      title: 'XII &ndash; Jinxed Lightlife Ends'
+    },
+    12: {
+      title: 'XIII &ndash; Subtropical Shrines'
+    },
+    13: {
+      title: 'XIV &ndash; Body Juts, Friezed'
+    },
+    14: {
+      title: 'XV &ndash; Flesh, Triumphal Grave'
+    },
+    15: {
+      title: 'XVI &ndash; The Fog Of Junk Psalms'
+    },
+    16: {
+      title: "XVII &ndash; About Plato's Cave"
+    },
+    17: {
+      title: 'XVIII &ndash; Darkness, Refresh View'
+    },
+    18: {
+      title: 'XIX &ndash; Hijacked Forms, Waves'
+    }
+  }
+}
---
+++ src/lambda/metadata.js
@@ -0,0 +1,92 @@
+import metadata from '../../metadata'
+require('dotenv').config()
+require('encoding') // netlify build error / missing package??
+const domain = process.env.VUE_APP_CANONICAL_DOMAIN
+
+// handler
+exports.handler = async function (event, context) {
+  try {
+    const tokenId = event.path.substr(event.path.lastIndexOf('/') + 1) // 19
+    // const networkId = event.queryStringParameters.network ?? '1' // ?network=4
+
+    let token = metadata.tokens[tokenId]
+
+    // !! token must exist in work.tokens list
+    if (!token) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'Not Found' })
+      }
+    }
+
+    // the sauce
+    token = {
+      // both opensea and rarebits
+      name: token.title, // || work.titlePattern.replace('{{no}}', printNo(work, tokenId)),
+
+      // owner: owner,
+      // name: `${doc.data.artist}, "${doc.data.title}", ${doc.data.year} (${printNo}/${doc.data.edition})`,
+
+      description: metadata.description, // work.description.replace('{{no}}', printNo(work, tokenId)), // by token ID?
+      // description: doc.data.description[0].text ?? '',
+
+      // all assets related to the work (posterity)
+      // directory: token.directory || work.directory,
+
+      // opensea
+      external_url: domain, // + '/works/' + workId + '/' + tokenId,
+      // rarebits
+      home_url: domain, // + '/works/' + workId + '/' + tokenId,
+
+      // opensea
+      image: `${domain}/metadata/${tokenId}.svg`,
+      // rarebits
+      image_url: `${domain}/metadata/${tokenId}.svg`,
+
+      // opensea
+      // attributes: [
+      //   {
+      //     trait_type: 'artist',
+      //     value: doc.data.artist
+      //   },
+      //   {
+      //     trait_type: 'year',
+      //     value: doc.data.year
+      //   }
+      // ],
+      // rarebits
+      // properties: [
+      //   { key: 'zodiac', value: returnZodiac(tokenId), type: 'string' }
+      // ],
+
+      // rarebits
+      // tags: ['cool', 'hot', 'mild']
+
+      // open sea
+      animation_url: `${domain}/metadata/${tokenId}.html`,
+
+      // optimized for folia site
+      // animation_url_optim: asset(work, tokenId, 'animation_url_optim'),
+      // animation_loop: token.animation_loop,
+
+      youtube_url: ''
+
+      // sha hashes for posterity (annoying for works with many files... IPFS is source file...)
+      // sha256: work.sha256 || {}
+    }
+
+    // return metadata :)
+    return {
+      statusCode: 200,
+      body: JSON.stringify(token)
+    }
+
+  // errors...
+  } catch (e) {
+    console.error(e)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ status: 500, message: 'Internal Server Error', error: e })
+    }
+  }
+}
