1517071cc352a7e2170fa33626965c2e5b0a3801
evvvritt <evv.info@gmail.com>
2021-06-02 21:10:57 +0200

    gray was purged?

--- tailwind.config.js
+++ tailwind.config.js
@@ -741,9 +741,9 @@ module.exports = {
         /-enter-active$/,
         /-leave-active$/,
         /-enter$/,
-        /-leave-to$/
+        /-leave-to$/,
         // content-dynamic classes
-        // /object-/,
+        /text-gray-/
         // /grid-cols-/
         // /^col-span-/,
         // /x12$/, // dynamic widths (w-5x12, md-w-6x12)
