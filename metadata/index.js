f09871d3932a5f92f6f92e2d13e60a637cb2c8b1
evvvritt <evv.info@gmail.com>
2021-06-02 14:10:14 +0200

    meta: add attr, dates

--- metadata/index.js
+++ metadata/index.js
@@ -8,21 +8,40 @@ export default {
   // assetPath: '',
   tokens: {
     0: {
-      title: 'I – Transfixed By Zion'
-    },
-    1: {
-      title: 'II – The Quickest Jury Now'
-    },
-    2: {
-      title: 'III – The Ark Groans, Zags Up'
-    },
-    3: {
-      title: 'IV – Hyperspectral Dawn',
-      date: '2021.06.06 07:06:28 -0400',
+      title: 'I – Transfixed By Zion',
+      date: '2021.06.05 20:24:26 -0400',
       attributes: [
         {
-          trait_type: 'Characters',
-          value: 60
+          trait_type: 'Letters',
+          value: 59
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 0
+        }
+      ]
+    },
+    1: {
+      title: 'II – The Quickest Jury Now',
+      date: '2021.06.05 23:04:56 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 53
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 2.6751708984375
+        }
+      ]
+    },
+    2: {
+      title: 'III – The Ark Groans, Zags Up',
+      date: '2021.06.06 01:45:27 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 64
         },
         {
           trait_type: 'Hours Locked',
@@ -30,50 +49,229 @@ export default {
         }
       ]
     },
+    3: {
+      title: 'IV – Hyperspectral Dawn',
+      date: '2021.06.06 07:06:28 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 60
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 10.70068359375
+        }
+      ]
+    },
     4: {
-      title: 'V – Zones Flex, Make New Ruins'
+      title: 'V – Zones Flex, Make New Ruins',
+      date: '2021.06.06 17:48:30 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 60
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 21.4013671875
+        }
+      ]
     },
     5: {
-      title: 'VI – Void Arrows, Zip Like'
+      title: 'VI – Void Arrows, Zip Like',
+      date: '2021.06.07 15:12:35 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 59
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 42.802734375
+        }
+      ]
     },
     6: {
-      title: 'VII – Jagged Quartz Towers'
+      title: 'VII – Jagged Quartz Towers',
+      date: '2021.06.09 10:00:45 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 59
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 85.60546875
+        }
+      ]
     },
     7: {
-      title: 'VIII – Verdant Perjury'
+      title: 'VIII – Verdant Perjury',
+      date: '2021.06.12 23:37:05 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 60
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 171.2109375
+        }
+      ]
     },
     8: {
-      title: 'IX – Nonreal Packet Maze'
+      title: 'IX – Nonreal Packet Maze',
+      date: '2021.06.20 02:49:44 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 62
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 342.421875
+        }
+      ]
     },
     9: {
-      title: 'X – Hacked Amazon'
+      title: 'X – Hacked Amazon',
+      date: '2021.07.04 09:15:03 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 53
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 684.84375
+        }
+      ]
     },
     10: {
-      title: 'XI – Quadratic Empire'
+      title: 'XI – Quadratic Empire',
+      date: '2021.08.01 20:05:41 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 54
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 1369.6875
+        }
+      ]
     },
     11: {
-      title: 'XII – Jinxed Lightlife Ends'
+      title: 'XII – Jinxed Lightlife Ends',
+      date: '2021.09.27 23:46:56 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 56
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 2739.375
+        }
+      ]
     },
     12: {
-      title: 'XIII – Subtropical Shrines'
+      title: 'XIII – Subtropical Shrines',
+      date: '2022.01.20 02:09:26 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 59
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 5478.75
+        }
+      ]
     },
     13: {
-      title: 'XIV – Body Juts, Friezed'
+      title: 'XIV – Body Juts, Friezed',
+      date: '2022.09.05 09:54:26 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 52
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 10957.5
+        }
+      ]
     },
     14: {
-      title: 'XV – Flesh, Triumphal Grave'
+      title: 'XV – Flesh, Triumphal Grave',
+      date: '2023.12.05 20:24:26 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 53
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 21915
+        }
+      ]
     },
     15: {
-      title: 'XVI – The Fog Of Junk Psalms'
+      title: 'XVI – The Fog Of Junk Psalms',
+      date: '2026.06.06 02:24:26 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 62
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 43830
+        }
+      ]
     },
     16: {
-      title: "XVII – About Plato's Cave"
+      title: "XVII – About Plato's Cave",
+      date: '2031.06.06 08:24:26 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 56
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 87660
+        }
+      ]
     },
     17: {
-      title: 'XVIII – Darkness, Refresh View'
+      title: 'XVIII – Darkness, Refresh View',
+      date: '2041.06.05 20:24:26 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 53
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 175320
+        }
+      ]
     },
     18: {
-      title: 'XIX – Hijacked Forms, Waves'
+      title: 'XIX – Hijacked Forms, Waves',
+      date: '2061.06.05 20:24:26 -0400',
+      attributes: [
+        {
+          trait_type: 'Letters',
+          value: 63
+        },
+        {
+          trait_type: 'Hours Locked',
+          value: 350640
+        }
+      ]
     }
   }
 }
