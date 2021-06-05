a72fc3719b0a2e29289e39285d9343a1c0c647f4
evvvritt <evv.info@gmail.com>
2021-06-05 18:44:56 +0200

    vue config split chunks

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  // webpack
+  configureWebpack: {
+    optimization: {
+      // split into smaller files for faster load?
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    }
+  }
 }
